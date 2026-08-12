9dd553cd26a3cbc33e29b70235e21e72ed88a15e
Ch-Ali-Ijaz <ch.ali55122@gmail.com>
2026-08-12 18:48:48 +0500

    Organize login Logic

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -2,13 +2,11 @@ import jwt from "jsonwebtoken";
 
 export function authenticate (req, res, next) {
     try {
-        const authHeader = req.headers.authorization;
+        const token = req.cookies.accessToken;
 
-        if(!authHeader){
+        if(!token){
             return res.status(401).json({msg: "No token provided"});
         }
-
-        const token = authHeader.split(' ')[1];
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./config/db.js";
 
 import loginRoutes from "./routes/loginRoutes.js";
 // import adminRoutes from "./routes/adminRoutes.js";
-// import testRoutes from "./routes/testRoutes.js";
 // import employeeRoutes from "./routes/employeeRoutes.js"
 
 
@@ -18,8 +18,8 @@ const PORT = 5001;
 
 connectDB();
 app.use(express.json());
+app.use(cookieParser());
 
-// app.use("/api/testRoutes", testRoutes);
 app.use("/api/login", loginRoutes);
 // app.use("/api/adminPortal", adminRoutes);
 // app.use("/api/employee", employeeRoutes);
