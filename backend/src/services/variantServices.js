d476497aefb6b45f0b141fccaf431d3593d2dfbc
Ch-Ali-Ijaz <ch.ali55122@gmail.com>
2026-09-17 23:16:03 +0500

    variantService now provides selling price to other services

--- backend/src/services/variantServices.js
+++ backend/src/services/variantServices.js
@@ -2,34 +2,22 @@ import Variant from "../models/Variant.js";
 import { setVariantId, setFilterObject } from "../utils/variantUtils.js";
 import { isVariantInputValid } from "../validators/variantValidators.js";
 
-export async function getAllVariants(queries){
-    try{
+export async function getVariants(queries) {
+    try {
         const filter = setFilterObject(queries);
         const variants = await Variant.find(filter);
-        
+
         return variants;
 
-    }catch(error){
+    } catch (error) {
         console.log("Error in getAllVariants service: ", error);
         throw new Error(error);
     }
 };
 
 // ---------------------------------------------------------------------------------------------------
-export async function getVariant(id){
-    try{
-        const variant = await Variant.findById(id);
-        return variant;
-
-    }catch(error){
-        console.log("Error in getVariant service: ", error);
-        throw new Error(error);
-    }
-};
-
-// ---------------------------------------------------------------------------------------------------
 export async function createVariant(productId, variantInfo) {
-    try{
+    try {
         isVariantInputValid(variantInfo);
 
         const variantId = setVariantId(variantInfo.ram, variantInfo.memory);
@@ -46,20 +34,20 @@ export async function createVariant(productId, variantInfo) {
             graphicCardMemory: Number(variantInfo.graphicCardMemory),
             storageType: variantInfo.storageType
         });
-        
+
         const variant = await newVariant.save();
-        
+
         return variant;
 
-    }catch(error){
+    } catch (error) {
         console.log("Error in createVariant service: ", error);
         throw new Error(error);
     }
 };
 
 // ---------------------------------------------------------------------------------------------------
-export async function updateVariant(id, newInfo){
-    try{
+export async function updateVariant(id, newInfo) {
+    try {
         isVariantInputValid(newInfo);
 
         const variant = {
@@ -75,41 +63,58 @@ export async function updateVariant(id, newInfo){
             graphicCardMemory: Number(newInfo.graphicCardMemory),
             storageType: newInfo.storageType
         };
-        const updatedVariant = await Variant.findByIdAndUpdate(id, variant, {returnDocument: "after"});
+        const updatedVariant = await Variant.findByIdAndUpdate(id, variant, { returnDocument: "after" });
 
         return updatedVariant;
 
-    }catch(error){
+    } catch (error) {
         console.log("Error in updateVariant service: ", error);
         throw new Error(error);
     }
 };
 
 // ---------------------------------------------------------------------------------------------------
-export async function setSellingPrice(id, sellingPrice, availableQuantity){
-    try{
-        const variant = {
-        availableQuantity: Number(availableQuantity),
-        sellingPrice: Number(sellingPrice)
+export async function getSellingPrice(variantId) {
+    try {
+        const variant = await Variant.findById(variantId);
+
+        if (!variant.sellingPrice) {
+            throw new Error(`Selling price not found against VariantId: ${variantId}`);
+        }
+
+        return variant.sellingPrice;
+
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error in getSellingPrice Service.");
     }
-    
-    return await Variant.updateOne(
-        {_id: id},
-        variant
-    );
-} catch(error){
-    console.log(error);
-    throw new Error("Error in setSellingPrice Service.");
-}
 };
 
 // ---------------------------------------------------------------------------------------------------
-export async function deleteVariant(id){
-    try{
+export async function setSellingPrice(objectId, sellingPrice, availableUnits) {
+    try {
+        const variant = {
+            availableUnits: Number(availableUnits),
+            sellingPrice: Number(sellingPrice)
+        }
+
+        return await Variant.updateOne(
+            { _id: objectId },
+            variant
+        );
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error in setSellingPrice Service.");
+    }
+};
+
+// ---------------------------------------------------------------------------------------------------
+export async function deleteVariant(id) {
+    try {
         const deletedProduct = await Variant.findByIdAndDelete(id);
         return deletedProduct;
 
-    }catch(error){
+    } catch (error) {
         console.log("Error in deleteVariant service: ", error);
         throw new Error(error);
     }
