a23022c2da2f1f17c4330b3cc297b652fe55ba2d
Ch-Ali-Ijaz <ch.ali55122@gmail.com>
2026-09-19 02:34:09 +0500

    getUserIds returns empty array if no criteria given

--- backend/src/services/userServices.js
+++ backend/src/services/userServices.js
@@ -38,8 +38,11 @@ export async function getUser(id) {
 // -------------------------------------------------------------------------------
 export async function getUserIds(queries) {
     try {
+        if(Object.keys(queries).length === 0){
+            return [];
+        }
+        
         const filter = setFilterObject(queries);
-
         const users = await User.find(filter);
         if(users.length === 0){
             throw new Error("No user found.");
