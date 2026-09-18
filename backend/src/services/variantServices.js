3dc39b1b9dfb35393ddac31e052ade3ba98e4efd
Ch-Ali-Ijaz <ch.ali55122@gmail.com>
2026-09-19 03:16:06 +0500

    Add getVariantIds to variantServices.js

--- backend/src/services/variantServices.js
+++ backend/src/services/variantServices.js
@@ -16,6 +16,25 @@ export async function getVariants(queries) {
 };
 
 // ---------------------------------------------------------------------------------------------------
+export async function getVariantIds(queries) {
+    try {
+        if(Object.keys(queries).length === 0){
+            return [];
+        }
+
+        const filter = setFilterObject(queries);
+        const variants = await Variant.find(filter);
+        const variantIds = variants.map(variant => variant._id);
+
+        return variantIds;
+
+    } catch (error) {
+        console.log("Error in getAllVariants service: ", error);
+        throw new Error(error);
+    }
+};
+
+// ---------------------------------------------------------------------------------------------------
 export async function createVariant(productId, variantInfo) {
     try {
         isVariantInputValid(variantInfo);
