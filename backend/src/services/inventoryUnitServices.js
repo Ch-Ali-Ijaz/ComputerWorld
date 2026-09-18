9c8da599260080684c8759c08129d6c766660b16
Ch-Ali-Ijaz <ch.ali55122@gmail.com>
2026-09-19 03:01:18 +0500

    Add getUnitIds to inventoryUnitServices.js

--- backend/src/services/inventoryUnitServices.js
+++ backend/src/services/inventoryUnitServices.js
@@ -24,6 +24,28 @@ export async function getUnits(queries) {
 };
 
 // -------------------------------------------------------------------------
+export async function getUnitIds(queries) {
+    try {
+        if(Object.keys(queries).length === 0){
+            return [];
+        }
+
+        const filter = setFilterObject(queries);
+        const units = await InventoryUnit.find(filter);
+        if(units.length === 0){
+            throw new Error("No such unit found.");
+        }
+        const unitIds = units.map(unit => unit._id);
+        
+        return unitIds;
+
+    } catch (error) {
+        console.log("Error in getAllUnits service: ", error);
+        throw new Error(error);
+    }
+};
+
+// -------------------------------------------------------------------------
 export async function getAvailableUnits(variantId, quantity) {
     try {
         const filter = setFilterObject({ variantId, currentStatus: "Available" });
